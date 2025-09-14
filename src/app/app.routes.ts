d1c0b561855199088b9911a92eb76f3c3a94a037
Priya-Vibhute <priyanka.vibhute@itvedant.com>
2025-09-14 10:28:40 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Product } from './product/product';
+import { Profile } from './profile/profile';
+import { AddProduct } from './add-product/add-product';
+import { ProductDetail } from './product-detail/product-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+   {path:"dashboard",component:Dashboard,title:"dashboard|project"},
+   {path:"products",component:Product,title:"products |project",children:[
+
+    {path:"add-product",component:AddProduct},
+    {path:"product-detail/1",component:ProductDetail}
+
+   ]},
+   {path:"profile",component:Profile,title:"profile | project"}
+];
